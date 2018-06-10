e1e381b5505f968563199df9e6fc75543d4773a2
Douglas <douglasmonteiro@icloud.com>
2018-06-10 00:39:56 -0300

    criação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ClientComponent } from './components/client/client.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AddressComponent } from './pages/address/address.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { WishlistComponent } from './pages/wishlist/wishlist.component';
+import { ProductComponent } from './components/product/product.component';
+import { WhishlistComponent } from './components/whishlist/whishlist.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    ProfileComponent,
+    AddressComponent,
+    OrdersComponent,
+    WishlistComponent,
+    ProductComponent,
+    WhishlistComponent
   ],
   imports: [
     BrowserModule
