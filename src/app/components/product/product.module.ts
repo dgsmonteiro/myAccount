ab9d77a449d4921b2a097d2c972c3572d02e6028
Douglas <douglasmonteiro@icloud.com>
2018-06-14 02:46:55 -0300

    adicionando pipe para filtrar produtos

--- src/app/components/product/product.module.ts
+++ src/app/components/product/product.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
+import { FiltraPorNomePipe } from './filtraPorNome.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    ProductComponent,
+    FiltraPorNomePipe
+  ],
+  exports: [
+    ProductComponent,
+    FiltraPorNomePipe
+  ]
 })
 export class ProductModule { }
